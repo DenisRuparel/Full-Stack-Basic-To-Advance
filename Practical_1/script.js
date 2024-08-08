f31c2115c759977fd3de72d099427aa9531b9d32
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2024-08-08 21:18:03 +0530

    tenth commit

--- Practical_1/script.js
+++ Practical_1/script.js
@@ -15,7 +15,6 @@ function updateVotes() {
     document.getElementById('javaVotes').textContent = votes.Java;
 }
 
-// Simulate real-time voting
 setInterval(() => {
     const languages = ['JavaScript', 'Python', 'Java'];
     const randomLanguage = languages[Math.floor(Math.random() * languages.length)];
